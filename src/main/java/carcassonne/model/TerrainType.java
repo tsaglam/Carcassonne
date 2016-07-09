f8b548777cc151bf9c3b3cb7bd5d80e4c322abff
Timur Saglam <uleay@student.kit.edu>
2016-07-09 21:56:10 +0200

    ADD: method for basic terrain and a special enum value.

--- src/main/java/carcassonne/model/TerrainType.java
+++ src/main/java/carcassonne/model/TerrainType.java
@@ -5,5 +5,14 @@ package carcassonne.model;
  * @author Timur Saglam
  */
 public enum TerrainType {
-    CASTLE, ROAD, MONASTRY, FIELDS, OTHER
+    CASTLE, ROAD, MONASTRY, FIELDS, OTHER, CASTLE_AND_ROAD;
+
+    /**
+     * Generates an array of the basic terrain types.
+     * @return an array of an array of CASTLE, ROAD, MONASTRY, FIELDS, OTHER.
+     */
+    public static TerrainType[] basicTerrain() {
+        TerrainType[] directions = { CASTLE, ROAD, MONASTRY, FIELDS, OTHER };
+        return directions;
+    }
 }
