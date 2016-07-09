e580160c10677f1e50393d98ed0f3bbc61ea8e8b
Timur Saglam <uleay@student.kit.edu>
2016-07-09 22:39:18 +0200

    CREATE: Small test case for the tile stack.

---
+++ src/test/java/tile/TileStackTest.java
@@ -0,0 +1,32 @@
+package tile;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import carcassonne.model.tile.TileStack;
+
+/**
+ * Test case for the tile stack class.
+ * @author Timur
+ */
+public class TileStackTest {
+    
+    TileStack stack;
+
+    @Before
+    public void setUp() throws Exception {
+        stack = new TileStack();
+    }
+
+    @Test
+    public void test() {
+        assertFalse(stack.isEmpty());
+        while (!stack.isEmpty()) {
+            System.out.println("Took " + stack.drawTile().getType() + " from stack.");
+        }
+        assertNull(stack.drawTile());
+    }
+
+}
