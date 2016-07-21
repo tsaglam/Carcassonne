373bb35f44b647aff770274d1e77ca53df2e2845
Timur Saglam <uleay@student.kit.edu>
2016-07-21 18:36:01 +0200

    EDIT: Made the constructor cleaner. ADD: Added a label grid and a method for building it.

--- src/main/java/carcassonne/view/MainGUI.java
+++ src/main/java/carcassonne/view/MainGUI.java
@@ -1,11 +1,14 @@
 package carcassonne.view;
 
 import java.awt.Color;
-import java.awt.Graphics;
-import java.awt.Image;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+
+import carcassonne.model.tile.Tile;
 
 /**
  * The main gui class, extending a JPanel.
@@ -14,31 +17,60 @@ import javax.swing.JPanel;
 public class MainGUI extends JPanel {
 
     private static final long serialVersionUID = -8750891542665009043L;
-    private JFrame mainFrame;
+    private JFrame frame;
     private MainMenuBar menuBar;
-    private Graphics g;
-    
+    private JLabel[][] labelGrid;
+    private GridBagConstraints constraints;
+
     /**
      * Basic constructor.
      */
-    public MainGUI() {
-        super();
+    public MainGUI(int frameWidth, int frameHeight) {
+        super(new GridBagLayout());
+        int gridWidth = frameWidth % 100;
+        int gridHeight = frameHeight % 100;
+        constraints = new GridBagConstraints();
         menuBar = new MainMenuBar();
-
-        mainFrame = new JFrame("CARC by Timur S.");
-        mainFrame.getContentPane().add(this);
-        mainFrame.setJMenuBar(menuBar);
-        mainFrame.setResizable(false);
-        mainFrame.setVisible(true);
-        mainFrame.setSize(1280, 768); // TODO make gui size variable
-        mainFrame.setLocationRelativeTo(null);
+        buildFrame(frameWidth, frameHeight);
         setBackground(new Color(165, 200, 245)); // background color
-
+        buildLabelGrid(gridWidth, gridHeight);
     }
-    
-    private void draw(Image image, int x, int y) {
-        g = getGraphics();
-        g.drawImage(image, x, y , this);
+
+    /**
+     * Draws the tile on the GUI.
+     * @param tile is the tile.
+     */
+    public void addTile(Tile tile) {
+        // TODO implement me.
+    }
+
+    /*
+     * Builds the frame.
+     */
+    private void buildFrame(int frameWidth, int frameHeight) {
+        frame = new JFrame("Carcasonne by Timur S.");
+        frame.getContentPane().add(this);
+        frame.setJMenuBar(menuBar);
+        frame.setResizable(false);
+        frame.setVisible(true);
+        frame.setSize(frameWidth, frameHeight); // TODO make gui size variable
+        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    /*
+     * Creates the grid of labels.
+     */
+    private void buildLabelGrid(int gridWidth, int gridHeight) {
+        labelGrid = new JLabel[gridWidth][gridHeight]; // build array of labels.
+        for (int x = 0; x < gridWidth; x++) {
+            for (int y = 0; y < gridHeight; y++) {
+                labelGrid[x][y] = new JLabel(); // create empty label
+                constraints.gridx = x; // set x coordinate.
+                constraints.gridy = y; // set y coordinate.
+                add(labelGrid[x][y], constraints); // add label with constraints
+            }
+        }
     }
 
     /**
@@ -46,7 +78,14 @@ public class MainGUI extends JPanel {
      * @param args are the arguments.
      */
     public static void main(String[] args) {
-        new MainGUI();
+        // TODO remove sometime
+        new MainGUI(1280, 768);
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.exit(0);
     }
 
 }
