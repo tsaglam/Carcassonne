cdcb3201102d5f8d12df8c9da729a875383a86bb
Timur Saglam <uleay@student.kit.edu>
2016-07-22 14:06:19 +0200

    EDIT: Some changes and fixes. ADD: Implemented the method for painting tiles to the grid.

--- src/main/java/carcassonne/view/MainGUI.java
+++ src/main/java/carcassonne/view/MainGUI.java
@@ -9,6 +9,8 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import carcassonne.model.tile.Tile;
+import carcassonne.model.tile.TileFactory;
+import carcassonne.model.tile.TileType;
 
 /**
  * The main gui class, extending a JPanel.
@@ -21,47 +23,58 @@ public class MainGUI extends JPanel {
     private MainMenuBar menuBar;
     private JLabel[][] labelGrid;
     private GridBagConstraints constraints;
+    private int gridWidth;
+    private int gridHeight;
 
     /**
      * Basic constructor.
+     * @param frameWidth is the width of the frame
+     * @param frameHeight is the height of the frame
      */
     public MainGUI(int frameWidth, int frameHeight) {
         super(new GridBagLayout());
-        int gridWidth = frameWidth % 100;
-        int gridHeight = frameHeight % 100;
         constraints = new GridBagConstraints();
-        menuBar = new MainMenuBar();
         buildFrame(frameWidth, frameHeight);
-        setBackground(new Color(165, 200, 245)); // background color
-        buildLabelGrid(gridWidth, gridHeight);
+        buildLabelGrid(frameWidth, frameHeight);
     }
 
     /**
-     * Draws the tile on the GUI.
+     * Draws the tile on a specific position on the GUI.
      * @param tile is the tile.
+     * @param x is the x coordinate.
+     * @param y is the y coordinate.
      */
-    public void addTile(Tile tile) {
-        // TODO implement me.
+    public void paint(Tile tile, int x, int y) {
+        if (x >= 0 && x < gridWidth && y >= 0 && y < gridHeight) {
+            labelGrid[x][y].setIcon(tile.getImage());
+        } else {
+            throw new IllegalArgumentException("Invalid label grid position (" + x + ", " + y + ")");
+        }
     }
 
     /*
      * Builds the frame.
      */
     private void buildFrame(int frameWidth, int frameHeight) {
+        menuBar = new MainMenuBar();
         frame = new JFrame("Carcasonne by Timur S.");
         frame.getContentPane().add(this);
         frame.setJMenuBar(menuBar);
         frame.setResizable(false);
         frame.setVisible(true);
-        frame.setSize(frameWidth, frameHeight); // TODO make gui size variable
+        frame.setSize(frameWidth, frameHeight);
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setBackground(new Color(165, 200, 245)); // background color
     }
 
     /*
      * Creates the grid of labels.
      */
-    private void buildLabelGrid(int gridWidth, int gridHeight) {
+    private void buildLabelGrid(int frameWidth, int frameHeight) {
+        gridWidth = frameWidth / 100;
+        gridHeight = frameHeight / 100;
+        System.out.println(gridWidth + " " + gridHeight);
         labelGrid = new JLabel[gridWidth][gridHeight]; // build array of labels.
         for (int x = 0; x < gridWidth; x++) {
             for (int y = 0; y < gridHeight; y++) {
@@ -77,9 +90,21 @@ public class MainGUI extends JPanel {
      * Main method for testing.
      * @param args are the arguments.
      */
-    public static void main(String[] args) {
-        // TODO remove sometime
-        new MainGUI(1280, 768);
+    public static void main(String[] args) { // TODO remove sometime
+        int x = 1280;
+        int y = 768;
+        MainGUI gui = new MainGUI(x, y);
+        Tile tile;
+        int rndX;
+        int rndY;
+        for (int i = 0; i < 50000; i++) {
+            for (TileType tileType : TileType.values()) {
+                tile = TileFactory.createTile(tileType);
+                rndX = (int) Math.round(Math.random() * ((x / 100) - 1));
+                rndY = (int) Math.round(Math.random() * ((y / 100) - 1));
+                gui.paint(tile, rndX, rndY);
+            }
+        }
         try {
             Thread.sleep(5000);
         } catch (InterruptedException e) {
