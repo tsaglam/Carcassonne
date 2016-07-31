c341e7228ebc5c9da7ee9ac0cd3a1dc709b3704c
Timur Saglam <uleay@student.kit.edu>
2016-07-31 16:12:08 +0200

    EDIT: Fixed main class.

--- src/main/java/carcassonne/CarcasonneMain.java
+++ src/main/java/carcassonne/CarcasonneMain.java
@@ -6,17 +6,12 @@ import carcassonne.control.MainController;
  * Carcassonne main class for starting the game itself.
  * @author Timur
  */
-public final class CarcasonneMain {
+public abstract class CarcasonneMain {
     /**
      * Main method for the Carcassonne game.
      * @param args are not used.
      */
     public static void main(String[] args) {
         new MainController();
-    }
-
-    // private constructor, should never be called.
-    private CarcasonneMain() {
-        throw new IllegalAccessError("This class mustn't be instantiated.");
     }
 }
