e47cd7fcb1a8f045e64fe9a62d18db706a93fa15
Timur Saglam <uleay@student.kit.edu>
2016-08-01 16:14:49 +0200

    CREATE: Made classes for the buttons and mouse adapters of the PlacementGUI.

---
+++ src/main/java/carcassonne/view/meepleButton/MeepleButton.java
@@ -0,0 +1,29 @@
+package carcassonne.view.meepleButton;
+
+import javax.swing.JButton;
+
+import carcassonne.control.MainController;
+import carcassonne.model.grid.GridDirection;
+
+/**
+ * Is a simple class derived form JButton, which stores (additionally to the JButton functions) the
+ * coordinates of the button on the button grid.
+ * @author Timur
+ */
+public class MeepleButton extends JButton {
+    private static final long serialVersionUID = -4580099806988033224L;
+
+    /**
+     * Simple constructor calling the <codeJButton>JButton()</code> constructor.
+     * @param controller is the controller of the GUI.
+     * @param x sets the x coordinate.
+     * @param y sets the y coordinate.
+     */
+    public MeepleButton(MainController controller, int x, int y) {
+        setup(controller, x, y);
+    }
+
+    private void setup(MainController controller, int x, int y) {
+        addMouseListener(new MeepleButtonMouseAdapter(GridDirection.values2D()[x][y], controller));
+    }
+}
---
+++ src/main/java/carcassonne/view/meepleButton/MeepleButtonMouseAdapter.java
@@ -0,0 +1,40 @@
+package carcassonne.view.meepleButton;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import carcassonne.control.MainController;
+import carcassonne.model.grid.GridDirection;
+
+/**
+ * The class is a specific mouse adapter for a <code>MeepleButton</code>. It connects the buttons
+ * with the <code>MainController</code>. The class is a subclass of
+ * <code>java.awt.event.MouseAdapter</code>.
+ * @author Timur
+ */
+public class MeepleButtonMouseAdapter extends MouseAdapter {
+
+    private GridDirection direction;
+    private MainController controller;
+
+    /**
+     * Basic constructor with the button and the controller to set.
+     * @param meepleButton should be the button that uses the adapter.
+     * @param controller sets the controller that is notified.
+     */
+    public MeepleButtonMouseAdapter(GridDirection direction, MainController controller) {
+        this.direction = direction;
+        this.controller = controller;
+    }
+
+    /**
+     * Method for processing mouse clicks on the <code>MeepleButton</code> of the class. notifies
+     * the <code>MainController</code> of the class.
+     * @param e is the <code>MouseEvent</code> of the click.
+     */
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        controller.requestMeeplePlacement(direction);
+    }
+
+}
