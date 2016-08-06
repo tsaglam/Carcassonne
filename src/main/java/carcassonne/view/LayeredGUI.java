0eed189e377ab97e332eaff5025163693442f9bc
Timur Saglam <uleay@student.kit.edu>
2016-08-06 12:16:22 +0200

    EDIT: Kinda got the components working, but only with fixed sizes.

--- src/main/java/carcassonne/view/LayeredGUI.java
+++ src/main/java/carcassonne/view/LayeredGUI.java
@@ -3,47 +3,60 @@ package carcassonne.view;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLayeredPane;
 import javax.swing.JPanel;
 
 import carcassonne.control.GameOptions;
+import carcassonne.control.MainController;
+import carcassonne.model.tile.Tile;
+import carcassonne.model.tile.TileFactory;
+import carcassonne.model.tile.TileType;
+import carcassonne.view.tileLabel.TileLabel;
 
 public class LayeredGUI {
     private JFrame frame;
     private JLayeredPane layeredPane;
-    private JPanel panelBack;
-    private JPanel panelFront;
+    private JPanel panelBottom;
+    private JPanel panelTop;
     private MainMenuBar menuBar;
     private GameOptions options;
+    // added from MainGUI:
+    private MainController controller;
+    private TileLabel[][] labelGrid;
+    private GridBagConstraints constraints;
+    private int gridWidth;
+    private int gridHeight;
 
-    public LayeredGUI() {
+    public LayeredGUI(MainController controller) {
+        this.controller = controller;
         options = GameOptions.getInstance();
+        constraints = new GridBagConstraints();
         // panel back:
-        panelBack = new JPanel();
-        panelBack.setSize(options.resolutionWidth, options.resolutionHeightWindow);
-        panelBack.setBackground(options.colorGUImain);
-        panelBack.setLayout(new GridBagLayout());
-        panelBack.add(new JButton()); // TODO remove
+        panelBottom = new JPanel();
+        panelBottom.setSize(options.gridResolutionWidth, options.gridResolutionHeight);
+        panelBottom.setBackground(options.colorGUImain);
+        panelBottom.setLayout(new GridBagLayout());
+        buildLabelGrid();
+        panelBottom.setSize(panelBottom.getMinimumSize());
+        panelBottom.revalidate();
         // panel front:
-        panelFront = new JPanel();
-        // TODO fix size:
-        panelFront.setSize(options.resolutionWidth - 100, options.resolutionHeightWindow - 100);
-        panelFront.setBackground(new Color(0, 0, 100, 100));
-        panelFront.setLayout(new GridBagLayout());
-        panelFront.add(new JButton()); // TODO remove
+        panelTop = new JPanel();
+        panelTop.setSize(options.gridResolutionWidth, options.gridResolutionHeight - 1);
+        panelTop.setBackground(new Color(0, 0, 100, 100));
+        panelTop.setLayout(new GridBagLayout());
         // layered pane:
         layeredPane = new JLayeredPane();
-        layeredPane.add(panelBack, 0, 0);
-        layeredPane.add(panelFront, 1, 0);
+        layeredPane.add(panelBottom, 0, 0);
+        layeredPane.add(panelTop, 1, 0);
         // frame building:
         frame = new JFrame();
         menuBar = new MainMenuBar();
         frame.setJMenuBar(menuBar);
-        frame.setPreferredSize(new Dimension(options.resolutionWidth, options.resolutionHeightWindow));
+        frame.setPreferredSize(new Dimension(options.gridResolutionWidth, options.resolutionHeightWindow - 29));
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new BorderLayout());
         frame.add(layeredPane, BorderLayout.CENTER);
@@ -58,11 +71,12 @@ public class LayeredGUI {
     }
 
     public static void main(String[] args) {
-        new LayeredGUI();
+        new LayeredGUI(null);
     }
 
     private void buildPanelBack() {
         // TODO (HIGHEST) use method.
+        buildLabelGrid();
     }
 
     private void buildPanelFront() {
@@ -75,6 +89,24 @@ public class LayeredGUI {
 
     private void buildFrame() {
         // TODO (HIGHEST) use method.
+    }
+
+    /*
+     * Creates the grid of labels.
+     */
+    private void buildLabelGrid() {
+        gridWidth = options.gridWidth;
+        gridHeight = options.gridHeight;
+        labelGrid = new TileLabel[gridWidth][gridHeight]; // build array of labels.
+        Tile defaultTile = TileFactory.create(TileType.Null);
+        for (int x = 0; x < gridWidth; x++) {
+            for (int y = 0; y < gridHeight; y++) {
+                labelGrid[x][y] = new TileLabel(defaultTile.getImage(), controller, x, y);
+                constraints.gridx = x;
+                constraints.gridy = y;
+                panelBottom.add(labelGrid[x][y], constraints); // add label with constraints
+            }
+        }
     }
 
 }
