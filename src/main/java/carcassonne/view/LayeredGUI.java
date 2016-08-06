9c09d794bfc58059487670d2aa658de0d058752a
Timur Saglam <uleay@student.kit.edu>
2016-08-06 11:19:58 +0200

    EDIT: Sorted the constructor, prepared private methods for later use in the constructor.

--- src/main/java/carcassonne/view/LayeredGUI.java
+++ src/main/java/carcassonne/view/LayeredGUI.java
@@ -1,6 +1,7 @@
 package carcassonne.view;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridBagLayout;
 
@@ -18,42 +19,62 @@ public class LayeredGUI {
     private JPanel panelFront;
     private MainMenuBar menuBar;
     private GameOptions options;
-    
+
     public LayeredGUI() {
         options = GameOptions.getInstance();
-        frame = new JFrame();
-        layeredPane = new JLayeredPane();
+        // panel back:
         panelBack = new JPanel();
-        panelFront = new JPanel();
-        frame.setPreferredSize(new Dimension(options.resolutionWidth, options.resolutionHeight));
-        frame.setLayout(new BorderLayout());
-        
-        menuBar = new MainMenuBar();
-        frame.setJMenuBar(menuBar);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLocationRelativeTo(null);
-        
-        
-        frame.add(layeredPane, BorderLayout.CENTER);   
-        
-        layeredPane.setSize(options.resolutionWidth, options.resolutionHeight);
-        panelBack.setSize(options.resolutionWidth, options.resolutionHeight);
-        panelBack.setOpaque(true);
+        panelBack.setSize(options.resolutionWidth, options.resolutionHeightWindow);
+        panelBack.setBackground(options.colorGUImain);
         panelBack.setLayout(new GridBagLayout());
         panelBack.add(new JButton()); // TODO remove
-        
-        panelFront.setSize(options.resolutionWidth, options.resolutionHeight);
-        panelFront.setOpaque(true);
+        // panel front:
+        panelFront = new JPanel();
+        // TODO fix size:
+        panelFront.setSize(options.resolutionWidth - 100, options.resolutionHeightWindow - 100);
+        panelFront.setBackground(new Color(0, 0, 100, 100));
         panelFront.setLayout(new GridBagLayout());
         panelFront.add(new JButton()); // TODO remove
+        // layered pane:
+        layeredPane = new JLayeredPane();
         layeredPane.add(panelBack, 0, 0);
         layeredPane.add(panelFront, 1, 0);
+        // frame building:
+        frame = new JFrame();
+        menuBar = new MainMenuBar();
+        frame.setJMenuBar(menuBar);
+        frame.setPreferredSize(new Dimension(options.resolutionWidth, options.resolutionHeightWindow));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new BorderLayout());
+        frame.add(layeredPane, BorderLayout.CENTER);
         frame.pack();
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+        // LATER:
+        buildPanelBack();
+        buildPanelFront();
+        buildLayeredPane();
+        buildFrame();
     }
 
     public static void main(String[] args) {
         new LayeredGUI();
+    }
+
+    private void buildPanelBack() {
+        // TODO (HIGHEST) use method.
+    }
+
+    private void buildPanelFront() {
+        // TODO (HIGHEST) use method.
+    }
+
+    private void buildLayeredPane() {
+        // TODO (HIGHEST) use method.
+    }
+
+    private void buildFrame() {
+        // TODO (HIGHEST) use method.
     }
 
 }
