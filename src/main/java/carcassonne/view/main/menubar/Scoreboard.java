5ad4835c9b9f987c8aad0e04954707bd2f0fccc5
Timur Saglam <uleay@student.kit.edu>
2016-08-17 19:42:43 +0200

    CREATE: Scoreboard class containing a score label for each player.

---
+++ src/main/java/carcassonne/view/main/menubar/Scoreboard.java
@@ -0,0 +1,74 @@
+package carcassonne.view.main.menubar;
+
+import javax.swing.JLabel;
+
+import carcassonne.control.GameOptions;
+
+/**
+ * Is the scoreboard class of the game. Manages a score label for each player.
+ * @author Timur Saglam
+ */
+public class Scoreboard {
+    private JLabel[] scoreLabel;
+    private final GameOptions options;
+
+    /**
+     * Standard constructor. Creates score board.
+     */
+    public Scoreboard() {
+        options = GameOptions.getInstance();
+        scoreLabel = new JLabel[options.maximalPlayers];
+        for (int i = 0; i < scoreLabel.length; i++) {
+            scoreLabel[i] = new JLabel("Player " + i + ": 0");
+            scoreLabel[i].setForeground(options.getPlayerColor(i));
+        }
+
+    }
+
+    /**
+     * Updates a label of the scoreboard.
+     * @param playerNumber is the number of the player whose label should get updated.
+     * @param points is the amount of points the players has.
+     */
+    public void update(int playerNumber, int points) {
+        scoreLabel[playerNumber] = new JLabel("Player " + playerNumber + ": " + points);
+    }
+
+    /**
+     * Only shows the specified amount of labels.
+     * @param playerCount is the amount of players to show labels for.
+     */
+    public void rebuild(int playerCount) {
+        int disabledPlayers = options.maximalPlayers - playerCount;
+        for (int i = disabledPlayers - 1; i < scoreLabel.length; i++) {
+            scoreLabel[i].setVisible(false);
+        }
+    }
+
+    /**
+     * Enables all the scoreboard labels.
+     */
+    public void enable() {
+        for (JLabel label : scoreLabel) {
+            label.setVisible(true);
+        }
+    }
+
+    /**
+     * Disables all the scoreboard labels.
+     */
+    public void disable() {
+        for (JLabel label : scoreLabel) {
+            label.setVisible(false);
+        }
+    }
+
+    /**
+     * Grants access to the labels themselves.
+     * @return the array of labels.
+     */
+    public JLabel[] getLabels() {
+        return scoreLabel;
+    }
+
+}
