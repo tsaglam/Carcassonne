a42f9cc272c873622efcaab9fb30be61ee74c355
Timur Saglam <uleay@student.kit.edu>
2016-08-18 11:36:51 +0200

    CREATE: Helper class for showing messages.

---
+++ src/main/java/carcassonne/view/secondary/GameMessage.java
@@ -0,0 +1,42 @@
+package carcassonne.view.secondary;
+
+import javax.swing.JOptionPane;
+
+/**
+ * Message class for showing the user small messages.
+ * @author Timur Saglam
+ */
+public final class GameMessage {
+
+    private GameMessage() {
+        // Private constructor for helper class.
+    }
+
+    /**
+     * Shows a custom plain message.
+     * @param messageText is the message text.
+     */
+    public static void showMessage(String messageText) {
+        show(messageText, JOptionPane.DEFAULT_OPTION);
+    }
+
+    /**
+     * Shows a custom warning message.
+     * @param messageText is the message text.
+     */
+    public static void showWarning(String messageText) {
+        show(messageText, JOptionPane.WARNING_MESSAGE);
+    }
+
+    /**
+     * Shows a custom error message.
+     * @param messageText is the message text.
+     */
+    public static void showError(String messageText) {
+        show(messageText, JOptionPane.ERROR_MESSAGE);
+    }
+
+    private static void show(String messageText, int type) {
+        JOptionPane.showMessageDialog(null, messageText, "Carcassonne", type);
+    }
+}
