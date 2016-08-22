8bfed2104c2b2cd017b61d3bd150b915bb5c14fd
Timur Saglam <uleay@student.kit.edu>
2016-08-22 14:51:45 +0200

    ADD: Nimbus LookAndFeel for non Mac OS systems.

--- src/main/java/carcassonne/CarcasonneMain.java
+++ src/main/java/carcassonne/CarcasonneMain.java
@@ -1,25 +1,37 @@
 package carcassonne;
 
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+
+import carcassonne.control.GameOptions;
 import carcassonne.control.MainController;
+import carcassonne.view.secondary.GameMessage;
 
 /**
  * Carcassonne main class for starting the game itself.
  * @author Timur Saglam
  */
 public final class CarcasonneMain {
-    /**
-     * Main method for the Carcassonne game.
-     * @param args are not used.
-     */
-    public static void main(String[] args) {
-        // try { // Simulate Windows Look and Feel
-        // UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
-        // } catch (Exception e) {
-        // e.printStackTrace();
-        // }
-        new MainController();
-    }
-    
-    private CarcasonneMain() {
-    }
+	/**
+	 * Main method for the Carcassonne game.
+	 * @param args are not used.
+	 */
+	public static void main(String[] args) {
+		if (!GameOptions.getInstance().operatingSystemName.startsWith("Mac")) {
+			try {
+				for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+					if ("Nimbus".equals(info.getName())) {
+						UIManager.setLookAndFeel(info.getClassName());
+						break;
+					}
+				}
+			} catch (Exception e) {
+				GameMessage.showError("Nimbus Look and Feel is not installed. Using default look and feel instead.");
+			}
+		}
+		new MainController();
+	}
+
+	private CarcasonneMain() {
+	}
 }
