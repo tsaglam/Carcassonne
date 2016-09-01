42526f0c04ca4768bb154400bd093292f03f0e6d
Timur Saglam <uleay@student.kit.edu>
2016-09-01 14:30:44 +0200

    CREATE: Added base classes for the AI algorithms.

---
+++ src/main/java/carcassonne/model/AbstractPlayerAI.java
@@ -0,0 +1,25 @@
+package carcassonne.model;
+
+import java.util.List;
+
+/**
+ * This is the abstract super class for the artificial intelligence classes. The artificial
+ * intelligence classes contain the algorithm that play the game independently.
+ * @author Timur Saglam
+ */
+public abstract class AbstractPlayerAI {
+
+    /**
+     * Basic constructor, should be called by sub classes.
+     */
+    public AbstractPlayerAI() {
+    }
+
+    public abstract CarcassonneMove getBestMove();
+
+    public List<CarcassonneMove> getPossibleMoves() {
+        // TODO (HIGHEST) implement method getPossibleMoves().
+        return null;
+    }
+
+}
---
+++ src/main/java/carcassonne/model/CarcassonneMove.java
@@ -0,0 +1,21 @@
+package carcassonne.model;
+
+/**
+ * A <code>CarcassonneMove</code> represents a single move a player makes to play a turn.
+ * @author Timur Saglam
+ */
+public class CarcassonneMove implements Comparable<CarcassonneMove> {
+
+    /**
+     * Basic constructor, creates the move object.
+     */
+    public CarcassonneMove() {
+        // TODO (HIGHEST) Implement CarcassonneMove class. Maybe build two subclasses.
+    }
+
+    @Override
+    public int compareTo(CarcassonneMove otherMove) {
+        // TODO (HIGHEST) Implement compareTo() method.
+        return 0;
+    }
+}
