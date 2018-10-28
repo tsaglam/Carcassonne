0d0dd2283dc6b79294c310673f795d8c372b2cb2
Timur Saglam <uleay@student.kit.edu>
2018-10-28 21:54:00 +0100

    EDIT: Used custom color chooser which shows a meeple preview.

--- src/main/java/carcassonne/view/main/menubar/MenuColorsMouseAdapter.java
+++ src/main/java/carcassonne/view/main/menubar/MenuColorsMouseAdapter.java
@@ -1,38 +1,85 @@
 package carcassonne.view.main.menubar;
 
-import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.HashMap;
+import java.util.Map;
 
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JColorChooser;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 
 import carcassonne.control.GameOptions;
+import carcassonne.control.PaintShop;
+import carcassonne.model.terrain.TerrainType;
 
 /**
- * A simple mouse adapter for the menu selection menu items.
+ * A mouse adapter for the selection of player colors. Listens to menu item clicks, event change and button pressing at
+ * the same time.
  * @author Timur Saglam
  */
-public class MenuColorsMouseAdapter extends MouseAdapter {
-    private final int player;
+public class MenuColorsMouseAdapter extends MouseAdapter implements ChangeListener, ActionListener {
+    private Map<TerrainType, JLabel> labelMap;
     private final GameOptions options;
+    private final PaintShop paintShop;
+    private final int player;
+    private final JColorChooser colorChooser;
 
     /**
      * Simple constructor.
-     * @param menuBar sets the menu bar.
-     * @param playerCount sets the amount of players.s
+     * @param player is the player whose color gets chosen.
      */
     public MenuColorsMouseAdapter(int player) {
         super();
         this.player = player;
         options = GameOptions.getInstance();
+        colorChooser = new JColorChooser();
+        colorChooser.getSelectionModel().addChangeListener(this);
+        colorChooser.setBorder(BorderFactory.createTitledBorder("Choose Meeple Color"));
+        paintShop = new PaintShop();
+        createPreview();
+
     }
 
+    // gets called when the ok button is pressed
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        options.setPlayerColor(colorChooser.getColor(), player);
+    }
+
+    // gets called when the menu entry is clicked.
     @Override
     public void mousePressed(MouseEvent e) {
-        Color newColor = JColorChooser.showDialog(null, "Choose Background Color", options.getPlayerColor(player));
-        if (newColor != null) { // TODO (HIGH) use own color picker
-            options.setPlayerColor(newColor, player);
+        colorChooser.setColor(options.getPlayerColor(player));
+        JColorChooser.createDialog(null, "TITLE", true, colorChooser, this, null).setVisible(true);
+    }
+
+    // gets called when the color is changed.
+    @Override
+    public void stateChanged(ChangeEvent e) {
+        for (TerrainType terrain : TerrainType.basicTerrain()) {
+            ImageIcon icon = paintShop.getColoredMeeple(terrain, colorChooser.getColor());
+            labelMap.get(terrain).setIcon(icon); // Update the preview labels.
         }
     }
 
+    /**
+     * Creates the preview panel of the color chooser.
+     */
+    private void createPreview() {
+        JPanel preview = new JPanel();
+        labelMap = new HashMap<>();
+        for (TerrainType terrain : TerrainType.basicTerrain()) {
+            JLabel label = new JLabel();
+            labelMap.put(terrain, label);
+            preview.add(label);
+        }
+        colorChooser.setPreviewPanel(preview);
+    }
 }
