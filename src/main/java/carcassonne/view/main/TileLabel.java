615ea519b3d59a056e501af6a425f4807b56123a
Timur Saglam <uleay@student.kit.edu>
2019-05-31 11:18:51 +0200

    EDIT: Implemented a first version of the tile selection highlighting.

---
+++ src/main/java/carcassonne/view/main/TileLabel.java
@@ -0,0 +1,83 @@
+package carcassonne.view.main;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.JLabel;
+
+import carcassonne.control.MainController;
+import carcassonne.model.tile.Tile;
+import carcassonne.model.tile.TileType;
+
+/**
+ * Is a simple class derived form JLabel, which stores (additionally to the JLabel functions) the coordinates of the
+ * label on the label grid.
+ * @author Timur Saglam
+ */
+public class TileLabel {
+    private Tile tile;
+    private final Tile defaultTile;
+    private final Tile highlightTile;
+    private final JLabel label;
+
+    /**
+     * Simple constructor calling the <codeJLabel>JLabel(ImageIcon image)</code> constructor.
+     * @param image sets the ImageIcon of the label.
+     * @param controller is the controller of the GUI.
+     * @param x sets the x coordinate.
+     * @param y sets the y coordinate.
+     */
+    public TileLabel(MainController controller, int x, int y) {
+        label = new JLabel();
+        defaultTile = new Tile(TileType.Null);
+        highlightTile = new Tile(TileType.Null);
+        defaultTile.rotateRight();
+        reset();
+        label.addMouseListener(new MouseAdapter() {
+            /**
+             * Method for processing mouse clicks on the <code>TileLabel</code> of the class. notifies the
+             * <code>MainController</code> of the class.
+             * @param e is the <code>MouseEvent</code> of the click.
+             */
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                controller.requestTilePlacement(x, y);
+            }
+
+            @Override
+            public void mouseEntered(MouseEvent event) {
+                toggleSelection();
+            }
+
+            @Override
+            public void mouseExited(MouseEvent event) {
+                toggleSelection();
+            }
+
+            private void toggleSelection() {
+                if (highlightTile.equals(tile)) {
+                    highlightTile.rotateRight();
+                    highlightTile.rotateRight();
+                    setTile(highlightTile);
+                }
+            }
+        });
+    }
+
+    public void setTile(Tile tile) {
+        this.tile = tile;
+        label.setIcon(tile.getImage());
+    }
+
+    public void highlight() {
+        setTile(highlightTile);
+    }
+
+    public void reset() {
+        setTile(defaultTile);
+    }
+
+    public JLabel getLabel() {
+        return label;
+    }
+}
