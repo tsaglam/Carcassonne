06371f49b96874d072437e23b62c7d48582a0268
Timur Saglam <uleay@student.kit.edu>
2020-05-09 13:39:27 +0200

    FIX: Fixed issue with the window maximization on Windows 10.

---
+++ src/main/java/carcassonne/view/main/WindowMaximizationAdapter.java
@@ -0,0 +1,35 @@
+package carcassonne.view.main;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+
+/**
+ * {@link WindowAdapter} that fixes an issue with proper window maximization on Windows 10. When adding this adapter to
+ * a {@link JFrame} it ensures its proper maximization (once) on activation. Just another little hack to fix Windows
+ * issues.
+ * @author Timur Saglam
+ */
+public class WindowMaximizationAdapter extends WindowAdapter {
+    private final JFrame frame;
+    private boolean maximized;
+
+    /**
+     * Creates the adapter for a specific {@link JFrame}
+     * @param is the specific {@link JFrame} to be maximized.
+     */
+    public WindowMaximizationAdapter(JFrame frame) {
+        this.frame = frame;
+        maximized = false;
+    }
+
+    @Override
+    public void windowActivated(WindowEvent event) {
+        if (!maximized) {
+            maximized = true;
+            frame.setExtendedState(frame.getExtendedState() | JFrame.MAXIMIZED_BOTH);
+        }
+
+    }
+}
