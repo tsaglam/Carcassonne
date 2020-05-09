79edb8b7df2000846025201f02e3dedb00918aa6
Timur Saglam <uleay@student.kit.edu>
2020-05-09 11:47:13 +0200

    ADD: Implemented a dialog to choose a custom grid size (currently disabled until the feature is fully implemented).

---
+++ src/main/java/carcassonne/view/GridSizeDialog.java
@@ -0,0 +1,94 @@
+package carcassonne.view;
+
+import java.awt.BorderLayout;
+import java.text.NumberFormat;
+
+import javax.swing.Box;
+import javax.swing.JFormattedTextField;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.text.NumberFormatter;
+
+import carcassonne.settings.GameSettings;
+
+/**
+ * Custom dialog for changing the grid size.
+ * @author Timur Saglam
+ */
+public class GridSizeDialog extends JPanel {
+
+    private static final int SPACE = 100;
+    private static final String CROSS = " x ";
+    private static final String HEIGHT = "Height:";
+    private static final int MAX_VALUE = 999;
+    private static final String MESSAGE = "Changes to the grid size will affect the next game. Choose values between 3 and 999.";
+    private static final int MIN_VALUE = 3;
+    private static final String NOT_CORRECT = " is not a valid grid sizes!";
+    private static final long serialVersionUID = 6533357898928866596L;
+    private static final int TEXT_FIELD_COLUMNS = 3;
+    private static final String TITLE = "Carcassonne";
+    private static final String WIDTH = "Width:";
+    private final GameSettings settings;
+    private final JTextField heightInput;
+    private final JTextField widthInput;
+
+    /**
+     * Creates a dialog to change the grid size.
+     * @param settings are the {@link GameSettings} that will receive the new grid size.
+     */
+    public GridSizeDialog(GameSettings settings) {
+        this.settings = settings;
+        widthInput = new JFormattedTextField(createNumberFormatter());
+        widthInput.setColumns(TEXT_FIELD_COLUMNS);
+        heightInput = new JFormattedTextField(createNumberFormatter());
+        heightInput.setColumns(TEXT_FIELD_COLUMNS);
+        setLayout(new BorderLayout());
+        add(new JLabel(MESSAGE), BorderLayout.NORTH);
+        JPanel subPanel = new JPanel();
+        subPanel.add(new JLabel(WIDTH));
+        subPanel.add(widthInput);
+        subPanel.add(Box.createHorizontalStrut(SPACE)); // a spacer
+        subPanel.add(new JLabel(HEIGHT));
+        subPanel.add(heightInput);
+        add(subPanel, BorderLayout.SOUTH);
+
+    }
+
+    /**
+     * Shows the grid size dialog and waits for the user input which is then sent to the game settings.
+     */
+    public void showDialog() {
+        widthInput.setText(Integer.toString(settings.getGridWidth()));
+        heightInput.setText(Integer.toString(settings.getGridHeight()));
+        int result = JOptionPane.showConfirmDialog(null, this, TITLE, JOptionPane.OK_CANCEL_OPTION, JOptionPane.DEFAULT_OPTION,
+                GameMessage.getGameIcon());
+        if (result == JOptionPane.OK_OPTION) {
+            processUserInput();
+        }
+    }
+
+    /**
+     * Parses the text input to valid integers and sends them to the settings.
+     */
+    private void processUserInput() {
+        try {
+            settings.setGridWidth(Integer.parseInt(widthInput.getText()));
+            settings.setGridHeight(Integer.parseInt(heightInput.getText()));
+        } catch (NumberFormatException exception) {
+            GameMessage.showWarning(widthInput.getText() + CROSS + heightInput.getText() + NOT_CORRECT);
+        }
+    }
+
+    /**
+     * Creates a number formatter that enforces the valid minimum and maximum values for the text fields.
+     */
+    private NumberFormatter createNumberFormatter() {
+        NumberFormatter formatter = new NumberFormatter(NumberFormat.getInstance());
+        formatter.setValueClass(Integer.class);
+        formatter.setMinimum(MIN_VALUE);
+        formatter.setMaximum(MAX_VALUE);
+        return formatter;
+    }
+}
--- src/main/java/carcassonne/view/menubar/MainMenuBar.java
+++ src/main/java/carcassonne/view/menubar/MainMenuBar.java
@@ -11,12 +11,11 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JRadioButtonMenuItem;
-import javax.swing.JSlider;
-import javax.swing.SwingConstants;
 
 import carcassonne.control.MainController;
 import carcassonne.settings.GameSettings;
 import carcassonne.settings.Notifiable;
+import carcassonne.view.GridSizeDialog;
 import carcassonne.view.main.MainGUI;
 
 /**
@@ -25,30 +24,31 @@ import carcassonne.view.main.MainGUI;
  */
 public class MainMenuBar extends JMenuBar implements Notifiable {
 
-    private static final String VIEW = "View";
-    private static final long serialVersionUID = -599734693130415390L;
-    private static final String GAME = "Game";
+    private static final String GRID_SIZE = "Change Grid Size";
     private static final String ABORT = "Abort Current Game";
-    private static final String NEW_ROUND = "Start New Round";
-    private static final String SETTINGS_OF = "Settings of ";
-    private static final String SETTINGS = "Player Settings";
-    private static final String PLAYERS = " Players";
     private static final String AMOUNT = "Amount of Players";
     private static final String CHAOS_MODE = "Enable Chaos Mode";
-    private static final String OPTIONS = "Options";
+    private static final String GAME = "Game";
     private static final String LARGE_SPACE = "          ";
+    private static final String NEW_ROUND = "Start New Round";
+    private static final String OPTIONS = "Options";
+    private static final String PLAYERS = " Players";
+    private static final long serialVersionUID = -599734693130415390L;
+    private static final String SETTINGS = "Player Settings";
+    private static final String SETTINGS_OF = "Settings of ";
+    private static final String VIEW = "View";
     private final MainController controller;
-    private final GameSettings settings;
+    private JMenuItem itemAbortRound;
+    private JMenuItem itemNewRound;
+    private JMenuItem[] itemSettings;
+    private final MainGUI mainUI;
     private JMenu menuGame;
     private JMenu menuOptions;
     private JMenu menuPlayers;
-    private JMenu menuView;
     private JMenu menuSettings;
-    private JMenuItem itemNewRound;
-    private JMenuItem itemAbortRound;
-    private JMenuItem[] itemSettings;
+    private JMenu menuView;
     private final Scoreboard scoreboard;
-    private final MainGUI mainUI;
+    private final GameSettings settings;
 
     /**
      * Simple constructor creating the menu bar.
@@ -62,11 +62,16 @@ public class MainMenuBar extends JMenuBar implements Notifiable {
         settings = controller.getSettings();
         settings.registerNotifiable(this);
         scoreboard = new Scoreboard(settings);
-        buildMenuGame();
-        buildMenuOptions();
+        buildGameMenu();
+        buildOptionsMenu();
         buildViewMenu();
         add(new JLabel(LARGE_SPACE));
         add(scoreboard);
+    }
+
+    public void enableStart() {
+        itemNewRound.setEnabled(true);
+        itemAbortRound.setEnabled(false);
     }
 
     /**
@@ -94,7 +99,7 @@ public class MainMenuBar extends JMenuBar implements Notifiable {
         }
     }
 
-    private void buildMenuGame() {
+    private void buildGameMenu() {
         itemNewRound = new JMenuItem(NEW_ROUND);
         itemAbortRound = new JMenuItem(ABORT);
         itemAbortRound.setEnabled(false);
@@ -107,7 +112,7 @@ public class MainMenuBar extends JMenuBar implements Notifiable {
         add(menuGame);
     }
 
-    private void buildMenuOptions() {
+    private void buildOptionsMenu() {
         buildPlayerMenu();
         buildSettingsMenu();
         notifyChange(); // set colors
@@ -117,11 +122,21 @@ public class MainMenuBar extends JMenuBar implements Notifiable {
         JCheckBoxMenuItem itemChaosMode = new JCheckBoxMenuItem(CHAOS_MODE);
         itemChaosMode.addMouseListener(new MouseAdapter() {
             @Override
-            public void mousePressed(MouseEvent e) {
+            public void mousePressed(MouseEvent event) {
                 settings.setChaosMode(!itemChaosMode.isSelected());
             }
         });
         menuOptions.add(itemChaosMode);
+        JMenuItem itemGridSize = new JMenuItem(GRID_SIZE);
+        itemGridSize.setEnabled(false); // TODO (HIGH) Enable when fully implemented, uncomment following lines.
+//        GridSizeDialog dialog = new GridSizeDialog(settings);
+//        itemGridSize.addMouseListener(new MouseAdapter() {
+//            @Override
+//            public void mousePressed(MouseEvent event) {
+//                dialog.showDialog();
+//            }
+//        });
+        menuOptions.add(itemGridSize);
         add(menuOptions);
     }
 
@@ -149,44 +164,11 @@ public class MainMenuBar extends JMenuBar implements Notifiable {
     }
 
     private void buildViewMenu() {
+        ZoomSlider slider = new ZoomSlider(mainUI);
         menuView = new JMenu(VIEW);
-        JSlider slider = new JSlider(50, 300, mainUI.getZoom());
-        slider.setPaintTicks(true);
-        slider.setOrientation(SwingConstants.VERTICAL);
-        slider.setMinorTickSpacing(5);
-        slider.setMajorTickSpacing(50);
-        slider.setSnapToTicks(true);
-        ZoomSliderListener zoomListener = new ZoomSliderListener(mainUI, slider);
-        slider.addMouseListener(zoomListener);
-        slider.addChangeListener(zoomListener);
-        JMenuItem zoomIn = new JMenuItem("Zoom In");
-        JMenuItem zoomOut = new JMenuItem("Zoom Out");
-        zoomIn.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mousePressed(MouseEvent event) {
-                mainUI.zoomIn();
-                zoomListener.setBlocked(true);
-                slider.setValue(mainUI.getZoom());
-                zoomListener.setBlocked(false);
-            }
-        });
-        zoomOut.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mousePressed(MouseEvent event) {
-                mainUI.zoomOut();
-                zoomListener.setBlocked(true);
-                slider.setValue(mainUI.getZoom());
-                zoomListener.setBlocked(false);
-            }
-        });
-        menuView.add(zoomIn);
+        menuView.add(slider.getZoomIn());
         menuView.add(slider);
-        menuView.add(zoomOut);
+        menuView.add(slider.getZoomOut());
         add(menuView);
-    }
-
-    public void enableStart() {
-        itemNewRound.setEnabled(true);
-        itemAbortRound.setEnabled(false);
     }
 }
