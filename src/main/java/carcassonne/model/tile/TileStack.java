0d7f4f44f7d757939f161489356d400da2b1c1f0
Timur Saglam <uleay@student.kit.edu>
2020-06-12 17:41:49 +0200

    EDIT: Removed traditional chaos mode from the game, implemented a central tile distribution managed by the settings.

--- src/main/java/carcassonne/model/tile/TileStack.java
+++ src/main/java/carcassonne/model/tile/TileStack.java
@@ -1,7 +1,5 @@
 package carcassonne.model.tile;
 
-import static java.util.stream.Collectors.toList;
-
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.Queue;
@@ -14,29 +12,17 @@ import java.util.Stack;
 public class TileStack {
     private final Stack<Tile> tiles;
     private final Queue<Tile> returnedTiles;
-    private Stack<Integer> randomAmounts;
     private final double multiplicator;
-    private final boolean useFixedAmounts;
 
     /**
      * Simple constructor, creates the default fixed-amount stack.
      * @param players is the amount of player for which this tile stack is intended.
      */
-    public TileStack(int players) {
-        this(players, false);
-    }
-
-    /**
-     * Constructor which allows specifying the stack type.
-     * @param players is the amount of player for which this tile stack is intended.
-     * @param useFixedAmounts specifies whether the amount of the different tile types should be fixed or random.
-     */
-    public TileStack(int players, boolean useFixedAmounts) {
-        this.useFixedAmounts = useFixedAmounts;
+    public TileStack(int players, TileDistribution distribution) {
         multiplicator = 0.4 + players * 0.3;
         tiles = new Stack<>();
         returnedTiles = new LinkedList<>();
-        fillStack();
+        fillStack(distribution);
         rotateRandomly();
         Collections.shuffle(tiles);
     }
@@ -85,52 +71,12 @@ public class TileStack {
         return tiles.isEmpty() && returnedTiles.isEmpty();
     }
 
-    private void fillStack() {
+    private void fillStack(TileDistribution distribution) {
         for (TileType tileType : TileType.validTiles()) {
-            int amount = getAmount(tileType); // call once per tile type
+            int amount = (int) (distribution.getQuantity(tileType) * multiplicator);
             for (int i = 0; i < amount; i++) {
                 tiles.add(new Tile(tileType));
             }
-        }
-    }
-
-    private int getAmount(TileType tileType) {
-        double amount;
-        if (useFixedAmounts) {
-            amount = Math.round(tileType.getAmount());
-        } else {
-            amount = getRandomAmount(tileType, 5);
-        }
-        return (int) (amount * multiplicator); // scale stack to player size
-    }
-
-    // assigns a somewhat random amount of tiles
-    private double getRandomAmount(TileType tileType, int shuffles) {
-        ensureInitialization();
-        int amount = randomAmounts.pop();
-        if (amount == tileType.getAmount()) { // if amount is the normal one
-            randomAmounts.push(amount); // put amount back
-            return getPseudoRandomAmount(tileType, shuffles - 1); // get pseudo random amount
-        }
-        return amount; // return random amount
-    }
-
-    // re-shuffles the stack and tries again. Use random number between 1 and 8 after a certain amount of tries.
-    private double getPseudoRandomAmount(TileType tileType, int shuffles) {
-        if (shuffles > 0) {
-            Collections.shuffle(randomAmounts);
-            return getRandomAmount(tileType, shuffles - 1);
-        } else {
-            return (int) (1 + Math.random() * 7);
-        }
-    }
-
-    // makes sure the random amounts are initialized.
-    private void ensureInitialization() {
-        if (randomAmounts == null) { // if random amounts where not generated
-            randomAmounts = new Stack<>(); // get all standard amounts in a stack
-            randomAmounts.addAll(TileType.validTiles().stream().map(it -> it.getAmount()).collect(toList()));
-            Collections.shuffle(randomAmounts);
         }
     }
 
