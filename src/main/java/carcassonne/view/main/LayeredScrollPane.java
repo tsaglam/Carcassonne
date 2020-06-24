211858d82272c0cf662596b74ad8ea4790114f26
Timur Saglam <uleay@student.kit.edu>
2020-06-24 22:16:08 +0200

    ADD: Added dedicated classes for the scroll pane and the tile and meeple layers.

---
+++ src/main/java/carcassonne/view/main/LayeredScrollPane.java
@@ -0,0 +1,74 @@
+package carcassonne.view.main;
+
+import java.awt.Component;
+import java.awt.Dimension;
+import java.awt.Point;
+import java.awt.Rectangle;
+import java.util.stream.Stream;
+
+import javax.swing.JLayeredPane;
+import javax.swing.JScrollPane;
+import javax.swing.OverlayLayout;
+
+/**
+ * {@link JScrollPane} that depicts the layers of a {@link JLayeredPane}. It is just a scroll pane that manages its own
+ * layered pane.
+ * @author Timur Saglam
+ */
+public class LayeredScrollPane extends JScrollPane {
+
+    private static final long serialVersionUID = 7863596860273426396L;
+    private static final int SCROLL_SPEED = 15;
+    private final JLayeredPane layeredPane;
+
+    /**
+     * Creates a layered scroll pane and centers it for a certain grid size.
+     */
+    public LayeredScrollPane() {
+        layeredPane = new JLayeredPane();
+        layeredPane.setLayout(new OverlayLayout(layeredPane));
+        setViewportView(layeredPane);
+        getVerticalScrollBar().setUnitIncrement(SCROLL_SPEED);
+        getHorizontalScrollBar().setUnitIncrement(SCROLL_SPEED);
+    }
+
+    /**
+     * Adds any number of components as layers in the given order.
+     * @param components the components to add as layers.
+     */
+    public void addLayers(Component... components) {
+        for (int i = 0; i < components.length; i++) {
+            layeredPane.add(components[i], i);
+        }
+    }
+
+    /**
+     * Removes any number of layers if they are part of this scroll pane.
+     * @param components are the components and therefore layers to remove.
+     */
+    public void removeLayers(Component... components) {
+        Stream.of(components).forEach(layeredPane::remove);
+    }
+
+    /**
+     * Repaint the layered pane and its layers.
+     */
+    public void repaintLayers() {
+        layeredPane.repaint();
+    }
+
+    /**
+     * Centers the scroll pane view to show the center of the grid. Make sure to call revalidate on the UI root before
+     * calling this method to avoid incorrect centering.
+     * @param gridWidth is the current grid with in tiles.
+     * @param gridHeight is the current height with in tiles.
+     * @param zoomLevel is the current zoom level and therefore also the tile size.
+     */
+    public void centerScrollPaneView(int gridWidth, int gridHeight, int zoomLevel) {
+        Dimension size = new Dimension(gridWidth * zoomLevel, gridHeight * zoomLevel);
+        Rectangle bounds = getViewport().getViewRect();
+        int x = (int) (Math.round((size.width - bounds.width) / 2.0));
+        int y = (int) (Math.round((size.height - bounds.height) / 2.0));
+        getViewport().setViewPosition(new Point(x, y));
+    }
+}
---
+++ src/main/java/carcassonne/view/main/TileLayer.java
@@ -0,0 +1,97 @@
+package carcassonne.view.main;
+
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+import carcassonne.control.MainController;
+import carcassonne.model.tile.Tile;
+import carcassonne.model.tile.TileType;
+import carcassonne.settings.GameSettings;
+
+/**
+ * A UI component that contains multiple tile panels.
+ * @author Timur Saglam
+ */
+public class TileLayer extends JPanel {
+    private static final long serialVersionUID = 1503933201337556131L;
+    private List<TileDepiction> tileLabels;
+    private TileDepiction[][] tileLabelGrid;
+
+    /**
+     * Creates the tile layer.
+     * @param controller is the main controller.
+     * @param gridWidth is the width of the grid in tiles.
+     * @param gridHeight is the height of the grid in tile.
+     * @param zoomLevel is the zoom level, and therefore the tile size.
+     */
+    public TileLayer(MainController controller, int gridHeight, int gridWidth, int zoomLevel) {
+        setBackground(GameSettings.GUI_COLOR);
+        setLayout(new GridBagLayout());
+        GridBagConstraints constraints = new GridBagConstraints();
+        tileLabels = new ArrayList<>();
+        tileLabelGrid = new TileDepiction[gridWidth][gridHeight]; // build array of labels.
+        Tile defaultTile = new Tile(TileType.Null);
+        Tile highlightTile = new Tile(TileType.Null);
+        defaultTile.rotateRight();
+        for (int x = 0; x < gridWidth; x++) {
+            for (int y = 0; y < gridHeight; y++) {
+                tileLabelGrid[x][y] = new TileDepiction(zoomLevel, defaultTile, highlightTile, controller, x, y);
+                tileLabels.add(tileLabelGrid[x][y]);
+                constraints.gridx = x;
+                constraints.gridy = y;
+                add(tileLabelGrid[x][y].getLabel(), constraints); // add label with constraints
+            }
+        }
+    }
+
+    /**
+     * Adapts the layer to a new zoom level.
+     * @param zoomLevel is the zoom level, and therefore also the tile size.
+     * @param preview determines if the tiles are rendered in preview mode (fast but ugly).
+     */
+    public void changeZoomLevel(int zoomLevel, boolean preview) {
+        Arrays.stream(tileLabelGrid).parallel().forEach(column -> IntStream.range(0, column.length) // columns
+                .forEach(i -> column[i].setTileSize(zoomLevel, preview))); // rows
+    }
+
+    /**
+     * Highlights a specific tile.
+     * @param x is the x-coordinate of that tile.
+     * @param y is the y-coordinate of that tile.
+     */
+    public void highlightTile(int x, int y) {
+        tileLabelGrid[x][y].highlight();
+    }
+
+    /**
+     * Places a tile, updating the correlating tile label.
+     * @param tile is the tile to place.
+     * @param x is the x-coordinate of that tile.
+     * @param y is the y-coordinate of that tile.
+     */
+    public void placeTile(Tile tile, int x, int y) {
+        tileLabelGrid[x][y].setTile(tile);
+    }
+
+    /**
+     * Refreshes the highlight image.
+     * @param newHighlight is the new image.
+     */
+    public void refreshHighlight(ImageIcon newHighlight) {
+        tileLabels.forEach(it -> it.setColoredHighlight(newHighlight));
+    }
+
+    /**
+     * Resets every tile label in this layer.
+     */
+    public void resetLayer() {
+        tileLabels.forEach(it -> it.reset());
+    }
+}
