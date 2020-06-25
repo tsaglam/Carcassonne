97590bbd3a779e54956db228528ec7d6dd8021de
Timur Saglam <uleay@student.kit.edu>
2020-06-25 18:12:07 +0200

    ADD: Helper interface that is a functional interface to listen to mouse clicks.

---
+++ src/main/java/carcassonne/util/MouseClickListener.java
@@ -0,0 +1,33 @@
+package carcassonne.util;
+
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+/**
+ * Utility interface that allows declaring functional mouse click listeners. By providing default methods for the other
+ * method signatures defined by {@link MouseListener}, this becomes a functional interface. This interface can be used
+ * like this: <code>addMouseListener((MouseClickListener)(e)->doSomething())</code>
+ * @author Timur Saglam
+ */
+public interface MouseClickListener extends MouseListener {
+
+    @Override
+    public default void mousePressed(MouseEvent event) {
+        // do nothing as only clicks are listened to.
+    }
+
+    @Override
+    public default void mouseReleased(MouseEvent event) {
+        // do nothing as only clicks are listened to.
+    }
+
+    @Override
+    public default void mouseEntered(MouseEvent event) {
+        // do nothing as only clicks are listened to.
+    }
+
+    @Override
+    public default void mouseExited(MouseEvent event) {
+        // do nothing as only clicks are listened to.
+    }
+}
