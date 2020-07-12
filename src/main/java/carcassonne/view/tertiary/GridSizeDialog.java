0d081e6f107fc3ec093e18d6a9092ed585fdc187
Timur Saglam <uleay@student.kit.edu>
2020-07-12 11:10:17 +0200

    EDIT: Extended grid size dialog to show a preview for the number of tiles. Additionally, invalid grid sizes are declared as such.

--- src/main/java/carcassonne/view/tertiary/GridSizeDialog.java
+++ src/main/java/carcassonne/view/tertiary/GridSizeDialog.java
@@ -1,6 +1,7 @@
 package carcassonne.view.tertiary;
 
 import java.awt.BorderLayout;
+import java.awt.Font;
 import java.text.NumberFormat;
 
 import javax.swing.Box;
@@ -8,7 +9,7 @@ import javax.swing.JFormattedTextField;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 import javax.swing.text.NumberFormatter;
 
 import carcassonne.settings.GameSettings;
@@ -20,6 +21,10 @@ import carcassonne.view.GameMessage;
  */
 public class GridSizeDialog extends JPanel {
 
+    private static final int GAP = 5;
+    private static final String BRACKET = ")";
+    private static final String TOTAL_TILES = "(Total tiles: ";
+    private static final String INVALID_SIZE = "Invalid grid size!";
     private static final int SPACE = 100;
     private static final String CROSS = " x ";
     private static final String HEIGHT = "Height:";
@@ -32,8 +37,9 @@ public class GridSizeDialog extends JPanel {
     private static final String TITLE = "Carcassonne";
     private static final String WIDTH = "Width:";
     private final GameSettings settings;
-    private final JTextField heightInput;
-    private final JTextField widthInput;
+    private final JFormattedTextField heightInput;
+    private final JFormattedTextField widthInput;
+    private final JLabel numberOfTiles;
 
     /**
      * Creates a dialog to change the grid size.
@@ -41,20 +47,26 @@ public class GridSizeDialog extends JPanel {
      */
     public GridSizeDialog(GameSettings settings) {
         this.settings = settings;
+        numberOfTiles = new JLabel();
+        numberOfTiles.setHorizontalAlignment(SwingConstants.CENTER);
+        Font font = new Font(numberOfTiles.getFont().getName(), Font.ITALIC, numberOfTiles.getFont().getSize());
+        numberOfTiles.setFont(font);
         widthInput = new JFormattedTextField(createNumberFormatter());
         widthInput.setColumns(TEXT_FIELD_COLUMNS);
         heightInput = new JFormattedTextField(createNumberFormatter());
         heightInput.setColumns(TEXT_FIELD_COLUMNS);
-        setLayout(new BorderLayout());
+        widthInput.addPropertyChangeListener(event -> updateNumberOfTiles());
+        heightInput.addPropertyChangeListener(event -> updateNumberOfTiles());
+        setLayout(new BorderLayout(GAP, GAP));
         add(new JLabel(MESSAGE), BorderLayout.NORTH);
         JPanel subPanel = new JPanel();
         subPanel.add(new JLabel(WIDTH));
         subPanel.add(widthInput);
-        subPanel.add(Box.createHorizontalStrut(SPACE)); // a spacer
+        subPanel.add(Box.createHorizontalStrut(SPACE));
         subPanel.add(new JLabel(HEIGHT));
         subPanel.add(heightInput);
-        add(subPanel, BorderLayout.SOUTH);
-
+        add(subPanel, BorderLayout.CENTER);
+        add(numberOfTiles, BorderLayout.SOUTH);
     }
 
     /**
@@ -63,10 +75,28 @@ public class GridSizeDialog extends JPanel {
     public void showDialog() {
         widthInput.setText(Integer.toString(settings.getGridWidth()));
         heightInput.setText(Integer.toString(settings.getGridHeight()));
+        updateNumberOfTiles(settings.getGridWidth(), settings.getGridHeight());
         int result = JOptionPane.showConfirmDialog(null, this, TITLE, JOptionPane.OK_CANCEL_OPTION, JOptionPane.DEFAULT_OPTION,
                 GameMessage.getGameIcon());
         if (result == JOptionPane.OK_OPTION) {
             processUserInput();
+        }
+    }
+
+    private void updateNumberOfTiles(int width, int height) {
+        int maxTiles = width * height;
+        numberOfTiles.setText(TOTAL_TILES + maxTiles + BRACKET);
+    }
+
+    private void updateNumberOfTiles() {
+        if (widthInput.isEditValid() && heightInput.isEditValid()) {
+            try {
+                updateNumberOfTiles(Integer.parseInt(widthInput.getText()), Integer.parseInt(heightInput.getText()));
+            } catch (NumberFormatException exception) {
+                numberOfTiles.setText(INVALID_SIZE);
+            }
+        } else {
+            numberOfTiles.setText(INVALID_SIZE);
         }
     }
 
@@ -87,6 +117,7 @@ public class GridSizeDialog extends JPanel {
      */
     private NumberFormatter createNumberFormatter() {
         NumberFormatter formatter = new NumberFormatter(NumberFormat.getInstance());
+        formatter.setCommitsOnValidEdit(true);
         formatter.setValueClass(Integer.class);
         formatter.setMinimum(MIN_VALUE);
         formatter.setMaximum(MAX_VALUE);
