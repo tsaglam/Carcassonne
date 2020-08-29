66c5b127c2cd2249b0b4c1c8b5b0c3727ede4e9b
Timur Saglam <timur.saglam@kit.edu>
2020-08-29 08:02:22 +0200

    EDIT: Utilized the fast scaling mechanic to improve the CTRL/CMD + mouse wheel scrolling.

---
+++ src/main/java/carcassonne/view/main/ZoomMode.java
@@ -0,0 +1,22 @@
+package carcassonne.view.main;
+
+/**
+ * Enumeration for the three zoom modes. Determines the image scaling quality and what images are scaled.
+ * @author Timur Saglam
+ */
+public enum ZoomMode {
+    /**
+     * Smooth image scaling, takes longer.
+     */
+    SMOOTH,
+
+    /**
+     * Fast image scaling, but with highlight scaling.
+     */
+    SEMI_FAST,
+
+    /**
+     * Fast image scaling, look not as good.
+     */
+    FAST;
+}
