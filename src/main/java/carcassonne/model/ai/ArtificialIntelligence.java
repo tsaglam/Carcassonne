73b518edd23f75baecdb6f65dc76bea64ec2dbba
Timur Saglam <timur.saglam@kit.edu>
2021-03-20 16:19:54 +0100

    ADD: Added prototypical rule-based a using zero sum moves.

---
+++ src/main/java/carcassonne/model/ai/ArtificialIntelligence.java
@@ -0,0 +1,26 @@
+package carcassonne.model.ai;
+
+import java.util.Collection;
+import java.util.Optional;
+
+import carcassonne.model.Player;
+import carcassonne.model.grid.Grid;
+import carcassonne.model.tile.Tile;
+
+public interface ArtificialIntelligence {
+    /**
+     * Returns the last calculated best move without recomputing it.
+     * @return
+     */
+    public Optional<CarcassonneMove> getCurrentMove();
+
+    /**
+     * Calculates a new best move for a player who places a tile on a grid. This move can be retrieved without recomputing
+     * it with {@link ArtificialIntelligence#getCurentMove()}
+     * @param tile is the set of tiles to choose from.
+     * @param player is the player who places the tile.
+     * @param grid is the grid on which the tile should be placed.
+     */
+    public Optional<CarcassonneMove> calculateBestMoveFor(Collection<Tile> tiles, Player player, Grid grid);
+
+}
