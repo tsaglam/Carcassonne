597b09ef9efe61e7c13e8d644c473b4afad8eb25
Timur Saglam <timur.saglam@kit.edu>
2021-03-27 15:23:04 +0100

    EDIT: Implemented a more sophisticated rule-based AI player.

--- src/main/java/carcassonne/model/ai/RuleBasedAI.java
+++ src/main/java/carcassonne/model/ai/RuleBasedAI.java
@@ -4,16 +4,22 @@ import static java.util.stream.Collectors.toList;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 import java.util.Random;
+import java.util.stream.Stream;
 
 import carcassonne.model.Player;
 import carcassonne.model.grid.Grid;
 import carcassonne.model.tile.Tile;
+import carcassonne.model.tile.TileStack;
 import carcassonne.settings.GameSettings;
 
 public class RuleBasedAI implements ArtificialIntelligence {
+    private static final int VALUE_THRESHOLD = 0;
+    private static final int FIELD_VALUE_THRESHOLD = 6;
+    private static final String EMPTY_COLLECTION = "Cannot choose random element from empty collection!";
     private final GameSettings settings;
     private Optional<CarcassonneMove> currentMove;
 
@@ -22,36 +28,58 @@ public class RuleBasedAI implements ArtificialIntelligence {
     }
 
     @Override
-    public Optional<CarcassonneMove> getCurrentMove() {
-        return currentMove;
-    }
-
-    @Override
-    public Optional<CarcassonneMove> calculateBestMoveFor(Collection<Tile> tiles, Player player, Grid grid) {
+    public Optional<CarcassonneMove> calculateBestMoveFor(Collection<Tile> tiles, Player player, Grid grid, TileStack stack) {
+        currentMove = Optional.empty();
         Collection<CarcassonneMove> possibleMoves = new ArrayList<>();
         for (Tile tile : tiles) {
             possibleMoves.addAll(grid.getPossibleMoves(tile, player, settings));
         }
-        possibleMoves.forEach(System.err::println); // TODO (HIGH remove debug output)
-        List<CarcassonneMove> consideredMoves = possibleMoves.stream().filter(it -> it.getValue() >= 0).collect(toList());
-        if (consideredMoves.size() == 0) {
-            currentMove = Optional.empty();
-        } else {
-            double maximumValue = consideredMoves.stream().mapToDouble(it -> it.getValue()).max().getAsDouble();
-            // TODO (HIGH) Count retrieved meeples in move
-            // TODO (HIGH) Maybe include using and retrieving meeples in score
-            // TODO (HIGH) Think about where to consider something like: don't place field when meeples <= 1
-            // TODO (HIGH) If there are multiple best options pick randomly (after monastery > castle > field > road)
-            currentMove = consideredMoves.stream().filter(it -> it.getValue() == maximumValue).findAny();
+        // RULE 1: Only consider move with a positive value:
+        List<CarcassonneMove> consideredMoves = possibleMoves.stream().filter(it -> it.getValue() >= VALUE_THRESHOLD).collect(toList());
+        // RULE 2: Do not place last meeple on fields
+        if (player.getFreeMeeples() == 1 && stack.getSize() > settings.getAmountOfPlayers()) {
+            consideredMoves = consideredMoves.stream().filter(it -> !it.isFieldMove()).collect(toList());
         }
-        System.out.println("Best: " + currentMove); // TODO (HIGH remove debug output)
+        // RULE 3: Do not place low value fields
+        consideredMoves = consideredMoves.stream().filter(it -> !isLowValueFieldMove(it)).collect(toList());
+        if (!consideredMoves.isEmpty()) {
+            currentMove = chooseBestMove(consideredMoves);
+        }
+        System.out.println(currentMove); // TODO (HIGH) remove debug output
         return currentMove;
     }
 
     @Override
-    public Tile chooseTileToDrop(Collection<Tile> tiles) { // TODO (HIGH) find better heuristic:
-        Optional<Tile> optionalTile = tiles.stream().skip(new Random().nextInt(tiles.size())).findFirst();
-        return optionalTile.orElseThrow(() -> new IllegalArgumentException("Cannot choose from empty collection!"));
+    public Tile chooseTileToDrop(Collection<Tile> tiles) {
+        return chooseRandom(tiles); // TODO (HIGH) find a meaningful heuristic
+    }
+
+    @Override
+    public Optional<CarcassonneMove> getCurrentMove() {
+        return currentMove;
+    }
+
+    private Optional<CarcassonneMove> chooseBestMove(List<CarcassonneMove> consideredMoves) {
+        double maximumValue = consideredMoves.stream().mapToDouble(it -> it.getValue()).max().getAsDouble();
+        Stream<CarcassonneMove> bestMoves = consideredMoves.stream().filter(it -> it.getValue() == maximumValue);
+        return chooseAmongBestMoves(bestMoves);
+    }
+
+    private Optional<CarcassonneMove> chooseAmongBestMoves(Stream<CarcassonneMove> bestMoves) {
+        RuleBasedComparator comparator = new RuleBasedComparator();
+        List<CarcassonneMove> listOfMoves = bestMoves.collect(toList());
+        Collections.sort(listOfMoves, comparator.reversed());
+        CarcassonneMove first = listOfMoves.get(0);
+        return Optional.of(chooseRandom(listOfMoves.stream().filter(it -> comparator.compare(it, first) == 0).collect(toList())));
+    }
+
+    private <T> T chooseRandom(Collection<T> elements) {
+        Optional<T> randomElement = elements.stream().skip(new Random().nextInt(elements.size())).findFirst();
+        return randomElement.orElseThrow(() -> new IllegalArgumentException(EMPTY_COLLECTION));
+    }
+
+    private boolean isLowValueFieldMove(CarcassonneMove move) {
+        return move.isFieldMove() && move.getValue() < FIELD_VALUE_THRESHOLD;
     }
 
 }
