77707ddecd3e34be39694487b60435ab24c2320f
Timur Saglam <timur.saglam@kit.edu>
2021-04-20 18:10:55 +0200

    FIX: Fixed the maximum tile resolution. It was previously not reduced to 500 when the tile resources were changed.

--- src/main/java/carcassonne/settings/GameSettings.java
+++ src/main/java/carcassonne/settings/GameSettings.java
@@ -25,7 +25,7 @@ public class GameSettings {
     public static final int MAXIMAL_PLAYERS = 5;
     public static final int MAXIMAL_TILES_ON_HAND = 5;
     public static final int TILE_SIZE = 100;
-    public static final int TILE_RESOLUTION = 600;
+    public static final int TILE_RESOLUTION = 500;
     public static final int HIGH_DPI_FACTOR = 2; // maximum supported DPI factor.
     public static final int MAXIMAL_MEEPLES = 7;
 
