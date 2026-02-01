050e19a2def12ffe6f166f1fa507ef4b8c4b73c7
Timur Saglam <4396919+tsaglam@users.noreply.github.com>
2026-02-01 12:01:25 +0100

    Adjust zoom steps to even pixels, limit maximal zoom level.

--- src/main/java/carcassonne/view/util/ZoomConfig.java
+++ src/main/java/carcassonne/view/util/ZoomConfig.java
@@ -4,11 +4,11 @@ package carcassonne.view.util;
  * Defines zoom configuration constants for the game view.
  */
 public enum ZoomConfig {
-    MAX_LEVEL(320),
+    MAX_LEVEL(256),
     MIN_LEVEL(16),
     DEFAULT_LEVEL(128),
-    STEP(25),
-    STEP_SMALL(5);
+    STEP(32),
+    STEP_SMALL(8);
 
     private final int pixels;
 
@@ -16,7 +16,7 @@ public enum ZoomConfig {
         this.pixels = pixels;
     }
 
-    /*
+    /**
      * @return the zoom level constant in pixels.
      */
     public int pixels() {
